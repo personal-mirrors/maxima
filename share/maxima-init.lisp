890348a94f6ab64911075b3beae38bd13c012a11
lbutler <lbutler>
2003-03-08 02:24:48 +0000

    This commit was manufactured by cvs2svn to create tag 'start'.

--- share/maxima-init.lisp
+++
@@ -1,8 +0,0 @@
-;;pre-allocating memory substantial improves GCL performance
-#+gcl
-(progn
-  (si::allocate-relocatable-pages 2000 t)
-  (si::allocate 'cfun 200 t)
-  (si::allocate 'fixnum 200 t)
-  (si::allocate 'cons 400 t)
-  (si::allocate 'symbol 100 t))
