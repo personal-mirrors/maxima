048e74f432668ea955221f07e8ad1d32072fa151
villate <villate>
2007-04-09 21:55:51 +0000

    Interprets lines beginning by ?? or ?! as manual searches and pass them to Maxima without waiting for a semicolon or dollar sign.

--- interfaces/xmaxima/Tkmaxima/RunMaxima.tcl
+++ interfaces/xmaxima/Tkmaxima/RunMaxima.tcl
@@ -1,6 +1,6 @@
 # -*-mode: tcl; fill-column: 75; tab-width: 8; coding: iso-latin-1-unix -*-
 #
-#       $Id: RunMaxima.tcl,v 1.29 2007-02-25 19:02:12 vvzhy Exp $
+#       $Id: RunMaxima.tcl,v 1.29.4.1 2007-04-09 21:55:51 villate Exp $
 #
 proc textWindowWidth { w } {
     set font [$w cget -font]
@@ -46,7 +46,7 @@ proc CMeval { w } {
     set expr [string trimright [$w get lastStart end] \n]
     # puts "command-line: ([$w index lastStart], [$w index end])"
     # puts "command: $expr"
-    if { ![regexp {^[ \n\t]*:|[;\$][ \t]*$|^\?[ \t]+[^ \t]} $expr] } {
+    if { ![regexp {^[ \n\t]*:|[;\$][ \t]*$|^\?[\?!]?[ \t]+[^ \t]} $expr] } {
 	$w insert insert "\n"
 	$w see insert
 	if { [catch {set atprompt [oget $w atMaximaPrompt]}] } {
