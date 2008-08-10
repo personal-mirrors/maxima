71835d4eb1770e96db8a9669913b45c5e4afae8e
robert_dodier <robert_dodier>
2008-08-10 19:15:52 +0000

    Remove TYPEP and COERCE redefinitions since they are causing trouble.

--- src/ecl-port.lisp
+++ src/ecl-port.lisp
@@ -20,21 +20,3 @@
       s)))
 
 (si::trap-fpe 'floating-point-underflow nil)
-
-;;;
-;;; The following optimizers are here because we sometimes run maxima
-;;; interpreted. They will change when some of these optimizations get
-;;; in ECL's compiler -- in that case we will simply reuse its code.
-;;;
-
-(defmacro maxima::typep (object type &environment env)
-  (let* ((whole (list 'cl:typep object type))
-	 (fd (compiler-macro-function 'cl:typep)))
-    (let* ((output (funcall fd whole env)))
-      (subst 'cl:typep 'typep output))))
-
-(defmacro coerce (&whole whole object type &environment env)
-  (let* ((whole (list* 'cl:coerce (rest whole)))
-	 (fd (compiler-macro-function 'cl:coerce)))
-    (let* ((output (funcall fd whole env)))
-      (subst 'cl:coerce 'coerce output))))
