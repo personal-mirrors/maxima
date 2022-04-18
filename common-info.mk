15aa68eb88e223eb172f650e83d3a4336c557c10
Gunter Königsmann <gunter@peterpall.de>
2022-04-18 09:45:37 +0200

    The .info files were expected to lie in the src folder

--- common-info.mk
+++ common-info.mk
@@ -36,7 +36,7 @@ uninstall-maxima-info:
 dist-hook: dist-maxima-info
 dist-maxima-info:
 	@srcdirstrip=`echo "$(srcdir)" | sed 's|.|.|g'`; \
-	list="$(srcdir)/$(infoname).info*" ; \
+	list="$(infoname).info*" ; \
 	for p in $$list; do \
 	  f=`echo "$$p" | sed "s|^$$srcdirstrip/||"`; \
 	  test -f $(distdir)/$$f || cp -p $(srcdir)/$$f $(distdir)/$$f; \
